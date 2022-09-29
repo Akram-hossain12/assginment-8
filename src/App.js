a60c84c0bd73327b7397c823320e25b09c8f6fe2
Akram-hossain12 <akramhossain0502@gmail.com>
2022-09-29 23:03:46 +0600

    foootr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Ans from './components/ans/Ans';
+import Footer from './components/footer/Footer';
 import Home from './components/Home/Home';
 import Nav from './components/nav/Nav';
 
@@ -12,6 +13,7 @@ function App() {
        <h2> Select todays Activity</h2>
        <Home></Home>
        <Ans></Ans>
+       <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <h5> @copyRight allright reservd by TheGruu 2022</h5>
+        </div>
+    );
+};
+
+export default Footer;
